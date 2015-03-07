70fbea31080a5d268bd373c0d84efa1526e1b230
Vikram Ramakrishnan <lord.ezar@gmail.com>
2015-03-07 14:48:29 -0500

    add process_url method to ElixirFbGetPush defmodule

--- lib/elixir_fb_get_push.ex
+++ lib/elixir_fb_get_push.ex
@@ -1,2 +1,11 @@
 defmodule ElixirFbGetPush do
+  use HTTPoison.Base
+
+  def process_url(date_as_string) do
+    "https://stock-indicator.firebaseio.com/" <> date_as_string
+  end
+
+  # https://stock-indicator.firebaseio.com/cvalues/2015-02-20.json
+  # curling above returns cvalues for feb 20, 2015 as a json
+
 end
